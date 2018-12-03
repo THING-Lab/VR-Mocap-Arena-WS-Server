1536c2530d11e39c34d2aeadb9b83dbf76d5fe04
Peter Gyory <ptgyory@gmail.com>
2018-12-03 10:21:13 -0700

    Inital Commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const http = require('http');
+const SocketIO = require('socket.io');
+const path = require('path');
+
+// Set up basic server stuff
+const app = express();
+const server = http.Server(app);
+const io = SocketIO(server);
+
+// Serve our `index.html` file
+app.get('*', (req, res) => {
+  const pathToHtml = path.resolve(__dirname, './index.html')
+  res.sendFile(pathToHtml);
+});
+
+// --- Start our server listening on port 3000 --- //
+
+server.listen(8000, () => {
+  console.log('Server listening on http://localhost:8000/');
+});
+
