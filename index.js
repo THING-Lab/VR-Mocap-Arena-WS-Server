a70222ea8cd18df0a08236bd8f252375cee8675b
Ryo Suzuki <ryosuzk@googlemail.com>
2019-05-10 11:04:21 -0600

    Add qtm parser

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const http = require('http');
 const SocketIO = require('socket.io');
 const path = require('path');
@@ -7,10 +8,12 @@ const qtmParser = require('./src/QualisysParser.js');
 
 // Set up basic server stuff
 const app = express();
+app.use(cors());
 const server = http.Server(app);
 const io = SocketIO(server);
 
 qtmParser(io);
+
 
 // Serve our `index.html` file
 app.get('*', (req, res) => {
@@ -18,7 +21,7 @@ app.get('*', (req, res) => {
   res.sendFile(pathToHtml);
 });
 
-// --- Start our server listening on port 3000 --- //
+// --- Start our server listening on port 8000 --- //
 
 server.listen(8000, () => {
   console.log('Server listening on http://localhost:8000/');
